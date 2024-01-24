ee198955b6568f1e22b342f7f4e707e207cbd53d
Dimitris Chios <chiosdimit@gmail.com>
2024-01-24 15:17:07 +0200

    fix the delete functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Note(props) {
+function Note({ index, title, content, onDelete }) {
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button>DELETE</button>
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button onClick={() => onDelete(index)}>DELETE</button>
     </div>
   );
 }
